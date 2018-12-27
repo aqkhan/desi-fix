fb72869de1fe4be833bba4b331bb034cdfa8ed49
A Q Khan <aqk.neo@gmail.com>
2018-12-28 02:57:31 +0500

    Put item with ASYNC AWAIT in hello func

--- episode-updater/handler.js
+++ episode-updater/handler.js
@@ -4,11 +4,11 @@ let sampleJson = require('./stub/dummyData.json');
 // Set the region 
 AWS.config.update({region: 'us-east-1'});
 
-module.exports.hello = (event) => {
+module.exports.hello = async (event) => {
   var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
   const newItem = AWS.DynamoDB.Converter.marshall(
     {
-      channelId: 'MARSHALLED02',
+      channelId: 'MARSHALLED03',
       string: 'foo',
       list: ['fizz', 'buzz', 'pop'],
       map: {
@@ -26,14 +26,8 @@ module.exports.hello = (event) => {
     TableName: "ChannelPlayLists",
     Item: newItem
   };
-  ddb.putItem(params, function(err, data) {
-    console.log('Inside hello');
-    if (err) {
-      console.log("Error", err);
-    } else {
-      console.log("Success", data);
-    }
-  });
+  let res = await ddb.putItem(params).promise();
+  console.log('Res: ', res)
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
