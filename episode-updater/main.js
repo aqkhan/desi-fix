d3e773bc7b48ebaa670e95756230f3e71421e834
A Q Khan <aqk.neo@gmail.com>
2018-12-27 22:28:36 +0500

    Dynamo DB setup

--- episode-updater/main.js
+++ episode-updater/main.js
@@ -1,6 +1,7 @@
 'use strict';
 const fetch = require('node-fetch');
-const url = `https://www.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=UC-lHJZR3Gqxm24_Vd_AJ5Yw&maxResults=25&key=AIzaSyDNOOFn5wDKlldZEwfomscJnZ7E9bM9c-0`;
+const channelId = `UC-lHJZR3Gqxm24_Vd_AJ5Yw`;
+const url = `https://www.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=${channelId}&maxResults=25&key=AIzaSyDNOOFn5wDKlldZEwfomscJnZ7E9bM9c-0`;
 
 module.exports.getPlaylists = async (event, context) => {
     const getData = async url => {
@@ -8,7 +9,11 @@ module.exports.getPlaylists = async (event, context) => {
             console.log('Inside try');
             const response = await fetch(url);
             const json = await response.json();
-            console.log(json.items);
+            const payLoad = {
+                uid: channelId,
+                items: json.items
+            };
+            console.log(payLoad);
         } catch (error) {
             console.log(error);
         }
