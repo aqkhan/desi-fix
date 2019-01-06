f724719893ef6f71f6a270b4d6cd4771e073db77
aqkhan <aqk.neo@gmail.com>
2019-01-06 16:10:30 +0500

    Stub home call

--- client-services/handler.js
+++ client-services/handler.js
@@ -12,3 +12,144 @@ module.exports.hello = async (event, context) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+
+module.exports.home = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      payLoad: {
+        channels: {
+          featured: {
+            trailer: true,
+            autoPlay: false,
+            thumbnail: '',
+            title: '',
+            layout: 1
+          },
+          liveChannels: [
+            {
+              uuid: '',
+              thumbnail: '',
+              title: '',
+              description: '',
+              source: 'yt',
+              link: ''
+            },
+            {
+              uuid: '',
+              thumbnail: '',
+              title: '',
+              description: '',
+              source: 'yt',
+              link: ''
+            },
+            {
+              uuid: '',
+              thumbnail: '',
+              title: '',
+              description: '',
+              source: 'yt',
+              link: ''
+            }
+          ],
+          dramas: [
+            {
+              uuid: '',
+              thumbnail: '',
+              title: '',
+              description: '',
+              source: 'yt',
+              link: '',
+              series: [
+                {
+                  uuid: '',
+                  thumbnail: '',
+                  title: '',
+                  description: '',
+                  source: 'yt',
+                  link: '',
+                  childOf: ''
+                }
+              ]
+            }
+          ],
+          movies: [
+            {
+              uuid: '',
+              thumbnail: '',
+              title: 'Lahore nhn jaon gi',
+              description: '',
+              source: 'yt',
+              link: ''
+            },
+            {
+              uuid: '',
+              thumbnail: '',
+              title: 'Teefa in trouble',
+              description: '',
+              source: 'yt',
+              link: ''
+            }
+          ],
+          news: [
+            {
+              uuid: '',
+              thumbnail: '',
+              title: '',
+              description: '',
+              source: 'yt',
+              link: ''
+            },
+            {
+              uuid: '',
+              thumbnail: '',
+              title: '',
+              description: '',
+              source: 'yt',
+              link: ''
+            },
+            {
+              uuid: '',
+              thumbnail: '',
+              title: '',
+              description: '',
+              source: 'yt',
+              link: ''
+            }
+          ],
+          misc: [
+            {
+              uuid: '',
+              thumbnail: '',
+              title: '',
+              description: '',
+              source: 'yt',
+              link: ''
+            },
+            {
+              uuid: '',
+              thumbnail: '',
+              title: '',
+              description: '',
+              source: 'yt',
+              link: ''
+            },
+            {
+              uuid: '',
+              thumbnail: '',
+              title: '',
+              description: '',
+              source: 'yt',
+              link: ''
+            }
+          ]  
+
+        }
+      }
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
