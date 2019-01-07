3d17d084af2f05e762cc258a533b7ff0172cb6dc
aqkhan <aqk.neo@gmail.com>
2019-01-07 17:06:29 +0500

    News & live channels complete

--- client-services/stub/data.js
+++ client-services/stub/data.js
@@ -10,28 +10,84 @@ module.exports.dummy = {
         },
         liveChannels: [
           {
-            uuid: '',
+            uuid: '1',
             thumbnail: '',
-            title: '',
+            title: 'Geo News',
             description: '',
             source: 'yt',
-            link: ''
+            link: 'https://www.youtube.com/watch?v=f0jX3dKBfzo'
           },
           {
-            uuid: '',
+            uuid: '2',
             thumbnail: '',
-            title: '',
+            title: 'Samaa TV',
             description: '',
             source: 'yt',
-            link: ''
+            link: 'https://www.youtube.com/watch?v=S4B1FBUpAg8'
           },
           {
-            uuid: '',
+            uuid: '3',
             thumbnail: '',
-            title: '',
+            title: '92 News',
             description: '',
             source: 'yt',
-            link: ''
+            link: 'https://www.youtube.com/watch?v=celjCYbotDA'
+          },
+          {
+            uuid: '4',
+            thumbnail: '',
+            title: 'News One',
+            description: '',
+            source: 'yt',
+            link: 'https://www.youtube.com/watch?v=BI8bKV478Bo'
+          },
+          {
+            uuid: '5',
+            thumbnail: '',
+            title: 'Hum News',
+            description: '',
+            source: 'yt',
+            link: 'https://www.youtube.com/watch?v=TYoz2AKc2nM'
+          },
+          {
+            uuid: '6',
+            thumbnail: '',
+            title: 'Express News',
+            description: '',
+            source: 'yt',
+            link: 'https://www.youtube.com/watch?v=EQvcoNUnixs'
+          },
+          {
+            uuid: '7',
+            thumbnail: '',
+            title: 'Bol News',
+            description: '',
+            source: 'yt',
+            link: 'https://www.youtube.com/watch?v=y4xJKSw_T0Y'
+          },
+          {
+            uuid: '8',
+            thumbnail: '',
+            title: 'Dawn News',
+            description: '',
+            source: 'yt',
+            link: 'https://www.youtube.com/watch?v=WHpra1Be_Us'
+          },
+          {
+            uuid: '9',
+            thumbnail: '',
+            title: 'GNN',
+            description: '',
+            source: 'yt',
+            link: 'https://www.youtube.com/watch?v=uC2CF61Cfwc'
+          },
+          {
+            uuid: '10',
+            thumbnail: '',
+            title: 'City 42',
+            description: '',
+            source: 'yt',
+            link: 'https://www.youtube.com/watch?v=Uxpoes_tois'
           }
         ],
         dramas: [
@@ -86,28 +142,28 @@ module.exports.dummy = {
         ],
         news: [
           {
-            uuid: '',
+            uuid: '11',
             thumbnail: '',
-            title: '',
+            title: 'Sky News',
             description: '',
             source: 'yt',
-            link: ''
+            link: 'https://www.youtube.com/watch?v=XOacA3RYrXk'
           },
           {
-            uuid: '',
+            uuid: '12',
             thumbnail: '',
-            title: '',
+            title: 'Al Jazeera',
             description: '',
             source: 'yt',
-            link: ''
+            link: 'https://www.youtube.com/watch?v=wp-R57ZaiXY'
           },
           {
-            uuid: '',
+            uuid: '13',
             thumbnail: '',
-            title: '',
+            title: 'Bloomberg',
             description: '',
             source: 'yt',
-            link: ''
+            link: 'https://www.youtube.com/watch?v=dp8PhLsUcFE'
           }
         ],
         misc: [
